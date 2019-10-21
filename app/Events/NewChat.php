52148938351147332ce2dfd4d1013291db72e026
rameez rami <ramees.pu@cubettech.com>
2019-10-21 15:51:35 +0530

    speed fix

--- app/Events/NewChat.php
+++ app/Events/NewChat.php
@@ -7,10 +7,11 @@ use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class NewChat implements ShouldBroadcast
+class NewChat implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
